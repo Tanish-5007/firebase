99745dd8d351df0a7e0ddb6c0142a0f4900d9975
Tanish <tanishkumar5007@gmail.com>
2021-07-20 02:13:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSave.setOnClickListener {
+            var note = editText.text.toString()
+            FirebaseDatabase.getInstance().reference.push().setValue(note)
+            Log.d("IDK", "onCreate: HELLOdsd")
+
+        }
+
+
+    }
+}
